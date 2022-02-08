bb4160cc8bbc1762df3c83b144c2a5d838b2e4bb
Frunza <DFrunza@luxoft.com>
2022-02-08 11:07:58 +0300

    feat: add django channels layer for communications and add test as well

--- taxi/routing.py
+++ taxi/routing.py
@@ -1,8 +1,16 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
+
+from trips.consumer import TaxiConsumer
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": URLRouter(
+            [
+                path("taxi/", TaxiConsumer.as_asgi()),
+            ]
+        ),
     }
 )
